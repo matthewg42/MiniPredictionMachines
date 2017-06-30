c792ef43fd6476d7bcb05b893279eaa8047a4752
Matthew Gates <matthewg42@gmail.com>
2017-06-30 12:39:11 +0100

    correct calculation of wind speed

--- src/weather_station/WindspeedSensor.cpp
+++ src/weather_station/WindspeedSensor.cpp
@@ -1,3 +1,4 @@
+#include <MutilaDebug.h>
 #include "WindspeedSensor.h"
 #include "Config.h"
 
@@ -5,6 +6,19 @@ PulseCounter WindspeedSensor(WINDSPEED_PIN);
 
 float calculateWindspeedMs(uint16_t pulseCount, uint32_t periodMs)
 {
-    return (WINSPEED_MS_PER_HZ*pulseCount*1000)/periodMs;
+    float hz = (pulseCount*1000.)/periodMs;
+    float windspeed = WINDSPEED_MS_PER_HZ*hz;
+    DB(F("calculateWindspeedMs time="));
+    DB(periodMs);
+    DB(F("ms pulses="));
+    DB(pulseCount);
+    DB(F(" hz="));
+    DB(hz);
+    DB('@');
+    DB(WINDSPEED_MS_PER_HZ);
+    DB(F(" m/s per hz = "));
+    DB(windspeed);
+    DBLN(F("m/s"));
+    return windspeed;
 }
 
