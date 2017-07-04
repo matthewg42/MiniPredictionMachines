c002b888aaad67cd70537a99218a56bc241e488b
Matthew Gates <matthewg42@gmail.com>
2017-07-04 12:24:02 +0100

    describe configuration items

--- src/mpm/Config.h
+++ src/mpm/Config.h
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 
+// Pins and hardware settings
 const uint8_t SW_UP_PIN                 = D0;
 const uint8_t SW_DOWN_PIN               = D4;
 const uint8_t HC12_SET_PIN              = D1;
@@ -13,12 +14,25 @@ const uint8_t OLED_SDA_PIN              = D5;
 const uint8_t HEARTBEAT_PIN             = D8;
 const bool HEARTBEAT_INV                = false;
 
+// Status messages and such
 const char SET_ACCT_UPD[] PROGMEM       = "Account Updated";
 const char SET_NTP_SERVER[] PROGMEM     = "Time Server";
-const uint16_t NTP_REFRESH_PERIOD_S     = 1800;
-const uint16_t NTP_RETRY_PERIOD_S       = 10;
-const uint16_t OLED_MESSAGE_PERIOD_S    = 61;
 
+// How often to try to sync time with NTP server
+const uint16_t NTP_REFRESH_PERIOD_S     = 1800;
+
+// When NTP fails, how long to wait before re-try
+const uint16_t NTP_RETRY_PERIOD_S       = 10;
+
+// How long after receipt of weather data we check for a new message
+const uint16_t OLED_MESSAGE_DELAY_S     = 8;
+
+// How many seconds to display a message for
+const uint16_t OLED_MESSAGE_BLANK_S     = 300;
+
+// Used to identify start of incoming weather data on HC12
 const uint8_t WEATHER_PACKET_MAGIC[]    = {0x12, 0x13};
+
+// How long to wait before discarding partially-received weather data
 const uint16_t PACKET_READ_TIMEOUT_MS   = 200;
 
