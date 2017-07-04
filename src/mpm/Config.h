647f5ab9e0a2767ee226a03100044c9949f95040
Matthew Gates <matthewg42@gmail.com>
2017-07-04 12:19:57 +0100

    prefer consts over #defines

--- src/mpm/Config.h
+++ src/mpm/Config.h
@@ -2,25 +2,23 @@
 
 #include <Arduino.h>
 
-#define     SW_UP_PIN               D0
-#define     SW_DOWN_PIN             D4
-#define     HC12_SET_PIN            D1
-#define     HC12_TX_PIN             D2
-#define     HC12_RX_PIN             D3
-#define     HC12_BAUD               4800
-#define     OLED_SCK_PIN            D6
-#define     OLED_SDA_PIN            D5
-#define     HEARTBEAT_PIN           D8
-#define     HEARTBEAT_INV           false
+const uint8_t SW_UP_PIN                 = D0;
+const uint8_t SW_DOWN_PIN               = D4;
+const uint8_t HC12_SET_PIN              = D1;
+const uint8_t HC12_TX_PIN               = D2;
+const uint8_t HC12_RX_PIN               = D3;
+const uint32_t HC12_BAUD                = 4800;
+const uint8_t OLED_SCK_PIN              = D6;
+const uint8_t OLED_SDA_PIN              = D5;
+const uint8_t HEARTBEAT_PIN             = D8;
+const bool HEARTBEAT_INV                = false;
 
-#define     SET_ACCT_UPD            F("Account Updated")
-#define     SET_NTP_SERVER          F("Time Server")
-#define     NTP_REFRESH_PERIOD_S    1800
-#define     NTP_RETRY_PERIOD_S      10
-#define     OLED_MESSAGE_PERIOD_S   61
+const char SET_ACCT_UPD[] PROGMEM       = "Account Updated";
+const char SET_NTP_SERVER[] PROGMEM     = "Time Server";
+const uint16_t NTP_REFRESH_PERIOD_S     = 1800;
+const uint16_t NTP_RETRY_PERIOD_S       = 10;
+const uint16_t OLED_MESSAGE_PERIOD_S    = 61;
 
 const uint8_t WEATHER_PACKET_MAGIC[]    = {0x12, 0x13};
 const uint16_t PACKET_READ_TIMEOUT_MS   = 200;
-
-
 
