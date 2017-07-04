9f6665c427ac786d0244cc33c9860dde015b44c8
Matthew Gates <matthewg42@gmail.com>
2017-07-04 14:11:04 +0100

    link OLED to oled example

--- src/mpm/OLED.cpp
+++ src/mpm/OLED.cpp
@@ -1,7 +1,1 @@
-#pragma once
-
-#include "OLED.h"
-#include "Config.h"
-
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C OLED(U8G2_R0, OLED_SCK_PIN, OLED_SDA_PIN, D0);
-
+../oled_test/OLED.cpp
--- src/mpm/OLED.h
+++ src/mpm/OLED.h
@@ -1,14 +1,1 @@
-#pragma once
-
-#include <U8g2lib.h>
-#include "Config.h"
-
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
-
-extern U8G2_SSD1306_128X64_NONAME_F_SW_I2C OLED;
-
+../oled_test/OLED.h
