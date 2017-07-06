47c85e053441a20b46a750d628f22210dc11c592
Matthew Gates <matthewg42@gmail.com>
2017-07-06 12:16:11 +0100

    remove ModeMessage from main

--- src/mpm/mpm.ino
+++ src/mpm/mpm.ino
@@ -20,10 +20,8 @@
 #include "SWDOWN.h"
 
 // Modes
-#include "ModeMessage.h"
 #include "ModeWeather.h"
 #include "ModeRealTime.h"
-//#include "ModeUploadData.h"
 
 // Other
 #include "EspID.h"
@@ -37,29 +35,28 @@ void setup()
     
     // Set ID based on mac address
     EspID.begin();
-    DB(F("Device ID="));
-    DBLN(EspID.get());
 
     // Initialize OLED and choose a font
     OLED.begin();
     OLED.clearBuffer();
-    OLED.setFont(u8g2_font_unifont_t_latin);
+    String s(F("Mini Prediction Machine ID: "));
+    s += EspID.get();
+    OLED.displayText(s.c_str(), 'C', 'M');
 
     HC12Serial.begin();
     delay(100);
 
     SWDOWN.begin();
 
-    ModeRealTime.begin();
     ModeWeather.begin();
-    ModeMessage.begin();
+    ModeRealTime.begin();
     EspApConfigurator.begin(SW_UP_PIN, HEARTBEAT_PIN, HEARTBEAT_INV);
 
     // Enable web server in ModeWifiClient - this just makes testing
     // easier as I don't have to keep connecting to the AP to see
     // how the interface is being rendered...
     //ModeWifiClient.enableHttpServer(true);
-    EspApConfigurator.addSetting(SET_ACCT_UPD,   new PersistentSettingBool(EspApConfigurator.nextFreeAddress(), false));
+    EspApConfigurator.addSetting(SET_ACCT_UPD,   new PersistentSettingBool(EspApConfigurator.nextFreeAddress(),   false));
     EspApConfigurator.addSetting(SET_NTP_SERVER, new PersistentSettingString(EspApConfigurator.nextFreeAddress(), 64, "time.google.com"));
 
     DBLN(F("E:setup"));
@@ -71,6 +68,5 @@ void loop()
     EspApConfigurator.update();
     ModeRealTime.update();
     ModeWeather.update();
-    ModeMessage.update();
 }
 
