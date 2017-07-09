d06f599ef726415aba2531b38e56fc235148412e
Matthew Gates <matthewg42@gmail.com>
2017-07-09 16:42:25 +0100

    implement with official names, return JSON

--- src/testing_api/lib/mpm/api.py
+++ src/testing_api/lib/mpm/api.py
@@ -5,12 +5,16 @@ import logging
 import subprocess
 import jsonmerge
 import random
+import hmac
+import hashlib
 from mpm.util import json_resp
 from flask import Blueprint, render_template, Response, request
 
 log = logging
 
 api = Blueprint('api', __name__)
+PRIVATE_KEY = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+PUBLIC_KEY = '1111111111'
 
 messages = ['It\'s not that easy being green',
 'Having to spend each day the color of the leaves.',
@@ -22,7 +26,11 @@ messages = ['It\'s not that easy being green',
 # net API route implementations
 @api.route('/current_message', methods=['GET'])
 def api_message():
-    return(messages[random.randint(0,len(messages)-1)])
+    try:
+        validate_params(request.args)
+        return(json_resp({'status': 'ok', 'message': messages[random.randint(0,len(messages)-1)]}))
+    except Exception as e:
+        return(json_resp({'status': 'error', 'error_message': str(e)}), 400)
 
 @api.route('/upload', methods=['POST'])
 def api_add():
@@ -35,10 +43,59 @@ def api_add():
         rh = request.form['rainfallMmMinute']
         rd = request.form['rainfallMmDay']
         bv = request.form['batteryVoltage']
-        print('api_add SUCCESS TE=%s WS=%s MO=%s RM=%s RH=%s RD=%s BV=%s' % (
-                te, ws, mo, rm, rh, rd, bv )) 
+        la = request.form['latitude']
+        lo = request.form['longitude']
+        print('api_add SUCCESS TE=%s WS=%s MO=%s RM=%s RH=%s RD=%s BV=%s LA=%s LO=%s' % (
+                te, ws, mo, rm, rh, rd, bv, la, lo )) 
     except Exception as e:
         return(json_resp({"error":"Internal server error", "message": "%s : %s" % (type(e), str(e))}, 400))
     return(json_resp({'status': 'ok'}))
 
+@api.route('/register', methods=['POST', 'GET'])
+def api_register():
+    return json_resp({'status': '/register TODO'})
+
+def calc_hmac(params):
+    values = [str(v) for k, v in params.items() if k != 'hmac']
+    values.sort()
+    print("values to hash: %s" % repr(values))
+    tohash = '&'.join(values) + '&' 
+    print("string to hash: %s" % tohash)
+    hmachex = hmac.new(PRIVATE_KEY.encode(), tohash.encode(), digestmod=hashlib.sha256).hexdigest()
+    print("hmac: %s" % hmachex)
+    return hmachex
+
+def validate_params(params):
+    ok = True
+    messages = []
+    for p in ['now', 'pubkey', 'did', 'dkey', 'hmac']:
+        if p not in params:
+            messages.append(p)
+            ok = False
+
+    if not ok:
+        raise Exception('Missing expected param(s): %s' % ', '.join(messages))
+    
+    try:
+        n = int(params['now'])
+    except Exception as e:
+        messages.append("param 'now' is not integer")
+        ok = False
+
+    if len(params['did']) != 6:
+        messages.append("param 'did' should be 6 characters long")
+        ok = False
+
+    if len(params['dkey']) != 10:
+        messages.append("param 'dkey' should be length 10")
+        ok = False
+
+    if calc_hmac(params) != params['hmac']:
+        messages.append("HMAC authentication failed")
+        ok = False
+
+    if not ok:
+        raise Exception('; '.join(messages))
+
+    return
 
--- src/testing_api/run.sh
+++ src/testing_api/run.sh
@@ -8,15 +8,15 @@ else
 
     echo GET /register
     GET "http://localhost:8888/register?now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=$RANDOM&hmac=ab4725c8f7e9898d7bb1231a" 
-    sleep 0.5
+    echo -e "\ncurl exit status=$?"; sleep 0.5
 
-    echo GET /current_message
-    GET "http://localhost:8888/current_message?now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=$RANDOM&hmac=ab4725c8f7e9898d7bb1231a"
-    echo ""
+    echo GET "http://localhost:8888/current_message?now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=0123456789&hmac=ab4725c8f7e9898d7bb1231a"
+    GET "http://localhost:8888/current_message?now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=0123456789&hmac=ab4725c8f7e9898d7bb1231a"
+    echo -e "\ncurl exit status=$?"; sleep 0.5
 
     echo POST upload
-    curl -q -X POST --data "now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=$RANDOM&hmac=ab4725c8f7e9898d7bb1231a&temperatureC=24.1&windspeedMs=1.2&moisture=0&rainfallMmHour=0.0&rainfallMmMinute=1.1&rainfallMmDay=3.5&batteryVoltage=4.45" "http://localhost:8888/upload"
-    sleep 0.5 
+    curl -q -X POST --data "now=$(date +%s)&pubkey=1234567890&did=EA12F5&dkey=$RANDOM&hmac=ab4725c8f7e9898d7bb1231a&temperatureC=24.1&windspeedMs=1.2&moisture=0&rainfallMmHour=0.0&rainfallMmMinute=1.1&rainfallMmDay=3.5&batteryVoltage=4.45&latitude=52.123&longitude=-1.234" "http://localhost:8888/upload"
+    echo -e "\ncurl exit status=$?"; sleep 0.5
     kill %%
 fi
 
